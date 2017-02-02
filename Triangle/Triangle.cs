1cba393a99070482f313f1dfc2d7f21dd17e3eb5
Joe Morris <joe.morris.dev@gmail.com>
2017-02-02 17:03:15 -0600

    triangle calculated

---
+++ Triangle/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Triangle
+{
+	public class Triangle
+	{
+		double height = 0.0;
+		double triangleBase = 0.0;
+
+		public void setArea() {
+			height = 8;
+			triangleBase = 10;
+		}
+
+		public double calculateArea()
+		{
+			return (height * triangleBase) / 2;
+		}
+
+		public void display() {
+			Console.WriteLine("Height: {0}", height);
+			Console.WriteLine("Base: {0}", triangleBase);
+			Console.WriteLine("Area : {0}", calculateArea());
+		}
+	}
+
+	public class executeTriangle{
+		public static void Main(string[] args) {
+			Triangle t = new Triangle();
+			t.setArea();
+			t.setArea();
+			t.display();
+		}
+	}
+}
